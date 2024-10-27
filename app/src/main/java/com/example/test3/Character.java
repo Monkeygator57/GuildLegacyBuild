8d66aaa4447ab89672a22c6323da7890396ffa36
Ethan LeFebvre <ethan@lefebvre.us>
2024-10-26 22:26:03 -0400

    Added functionality for characters to take advantage of equipment.

--- app/src/main/java/com/example/test3/Character.java
+++ app/src/main/java/com/example/test3/Character.java
@@ -7,23 +7,41 @@ public abstract class Character {
     protected int health;
     protected int attackPower;
     protected int defense;
+    protected Weapon equippedWeaon;
+    protected Armor equippedArmor;
 
     public Character(String name, int health, int attackPower, int defense) {
         this.name = name;
         this.health = health;
         this.attackPower = attackPower;
         this.defense = defense;
+        this.equippedArmor = null;
+        this.equippedWeaon = null;
     }
 
     // Method for attack
     public abstract void attack(Character target);
 
-    // Method to take damage
+
+    // Modified takeDamage method to use total defense
     public void takeDamage(int damage) {
-        int damageTaken = Math.max(damage - defense, 0); // Damage Can't be Negative
+        int damageTaken = Math.max(damage - getTotalDefense(), 0);
         health -= damageTaken;
         System.out.println(name + " takes " + damageTaken + " damage! Health now: " + health);
     }
+
+
+    // Equipment methods
+    public void equipWeapon(Weapon weapon) {
+        this.equippedWeapon = weapon;
+        System.out.println(name + " equipped " + weapon.getName());
+    }
+
+    public void equipArmor(Armor armor) {
+        this.equippedArmor = armor;
+        System.out.println(name + " equipped " + armor.getName());
+    }
+
 
     // Check if character is alive
     public boolean isAlive() {
@@ -45,5 +63,22 @@ public abstract class Character {
 
     public int getAttackPower() {
         return attackPower;
+    }
+
+    // Modified getters to include equipment bonuses
+    public int getTotalAttackPower() {
+        int totalAttack = attackPower;
+        if (equippedWeapon != null) {
+            totalAttack += equippedWeapon.getAttackBonus();
+        }
+        return totalAttack;
+    }
+
+    public int getTotalDefense() {
+        int totalDefense = defense;
+        if (equippedArmor != null) {
+            totalDefense += equippedArmor.getDefenseBonus();
+        }
+        return totalDefense;
     }
 }
