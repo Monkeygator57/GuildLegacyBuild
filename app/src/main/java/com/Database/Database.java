27f03145a998a7411f15c239249b892952fad57b
BuildTools <unconfigured@null.spigotmc.org>
2024-11-19 18:58:58 -0500

    Database implemented to login system

--- app/src/main/java/com/Database/Database.java
+++ app/src/main/java/com/Database/Database.java
@@ -1,18 +1,31 @@
 package com.Database;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import static java.lang.String.valueOf;
+
 
 public class Database {
 
     //Global Variables
     public static ArrayList<ArrayList<String>> Database = new ArrayList<ArrayList<String>>();
-    public static File myObj = new File("Data.txt");
+    public String filename;
+    public File path;
+
+
+    public Database(String filetxt, File Path) throws IOException {
+        filename = filetxt;
+        path = Path;
+        File newFile = new File(path, filename);
+        if (!newFile.exists()){
+            newFile.createNewFile();
+        }
+
+
+
+
+    }
 
     public static void database(String[] args) throws FileNotFoundException {
 
@@ -20,11 +33,12 @@ public class Database {
 
     //Query method
     // Use Query to retrieve any information in a record. returnNum is the index of the value you want
-    // 0:UserID, 1:Warrior Level, 2:Mage Level, 3:Cleric Level, 4:Rogue Level, 5:Equipped Item
+    // Note** INDEX LOCATIONS FOR QUERY METHOD
+    // 0:Username|1:Password|2:ClassLevel|3:EquippedWeapon|4:EquippedArmor|5:EquippedTrinket
     // After these, every even number is an item and the following odd number is the weight
-    public String Query(int UserID, int returnNum) {
+    public String Query(String UserID, int returnNum) {
         for (ArrayList<String> Record : Database) {
-            if (Record.get(0).equalsIgnoreCase(valueOf(UserID))) {
+            if (Record.get(0).equalsIgnoreCase(UserID)) {
                 return Record.get(returnNum);
             }
         }
@@ -32,20 +46,19 @@ public class Database {
     }
 
     // Changing the Class Levels
-    // Index of 1   2      3          4
-    // Is Warrior, Mage, Cleric, and Rogue
-    // Int Level refers to the index of the level, Value refers to the actual value of it
-    public void SetLevel(int ID, int Level, int value){
-        Database.get(GetIndex(ID)).set(Level, valueOf(value));
+    //Username and level to set class level to
+    public void SetLevel(String ID, int value){
+        Database.get(GetIndex(ID)).set(2, valueOf(value));
     }
-    public void LevelUp(int ID, int level){
-        int value = Integer.parseInt(Database.get(GetIndex(ID)).get(level));
+    //Increases Class Level by one
+    public void LevelUp(String ID){
+        int value = Integer.parseInt(Database.get(GetIndex(ID)).get(2));
         value++;
-        Database.get(GetIndex(ID)).set(level, valueOf(value));
+        Database.get(GetIndex(ID)).set(2, valueOf(value));
     }
 
     //New user method
-    public void NewRecord(int ID) {
+    public void NewRecord(String ID, String password) throws FileNotFoundException {
         //Checks if this ID exists
         for (ArrayList<String> Record : Database) {
             if (Record.get(0).equalsIgnoreCase(valueOf(ID))) {
@@ -54,20 +67,22 @@ public class Database {
             }
         }
         ArrayList<String> newUser = new ArrayList<String>();
-        newUser.add(valueOf(ID));
-        newUser.add("0"); //Warrior Level
-        newUser.add("0"); //Mage Level
-        newUser.add("0"); //Cleric Level
-        newUser.add("0"); //Rogue Level
-        newUser.add("NA"); //Equipped Item
+        newUser.add(ID);
+        newUser.add(password);
+        newUser.add("NA"); //Equipped Weapon
+        newUser.add("NA"); //Equipped Armor
+        newUser.add("NA"); //Equipped Trinket
         Database.add(newUser);
+        UpdateData();
     }
 
     //Writes database onto file and updates database array
     public void UpdateData() throws FileNotFoundException {
-        Scanner myReader = new Scanner(myObj);
+        File file = new File(path, filename);
+        Scanner myReader = new Scanner(file);
         try {
-            FileWriter myWriter = new FileWriter("Data.txt");
+            FileWriter myfWriter = new FileWriter(file);
+            BufferedWriter myWriter = new BufferedWriter(myfWriter);
             for (int d = 0; d < Database.size(); d++) {
                 for (int k = 0; k < Database.get(d).size(); k++) {
                     myWriter.write(Database.get(d).get(k) + '|');
@@ -88,11 +103,11 @@ public class Database {
     }
 
 
-    //Note: Inventory will start at an index of 6
-    // Index of 5 Is Equipped Item
+    //Note: Inventory will start at an index of 5
+    // Index of 2,3,4 Is Equipped Item
 
     //Adds a list of items to the inventory
-    public void InventoryAddList(int ID, ArrayList<String> items) {
+    public void InventoryAddList(String ID, ArrayList<String> items) {
         if (items.size() % 2 == 0) { //Checking that there are an even number of items in the list to insure each inventory item as a weight
             for (String item : items) {
                 Database.get(GetIndex(ID)).add(item);
@@ -100,21 +115,54 @@ public class Database {
         }
     }
     //Adds an individual Item to the Inventory
-    public void InventoryAddItem(int ID, String item, int weight) {
+    public void InventoryAddItem(String ID, String item, int weight) {
         Database.get(GetIndex(ID)).add(item);
         Database.get(GetIndex(ID)).add(valueOf(weight));
     }
     //Returns Currently Equipped Item
-    public String GetEquippedItem(int ID) {
-        return Database.get(GetIndex(ID)).get(5);
+    public String GetEquippedItem(String ID, String Item) {
+        int equippedItemIndex = -1;
+        // Used for QoL, Allows you to enter the location you want changed rather than a number
+        switch(Item){
+            case "hand":
+                equippedItemIndex = 2;
+                break;
+            case "armour":
+                equippedItemIndex = 3;
+                break;
+            case "trinket":
+                equippedItemIndex = 4;
+                break;
+        }
+        if (Database.get(GetIndex(ID)).get(equippedItemIndex).equalsIgnoreCase("NA")){ //Checks if there is an equipped item
+            return null;
+        } else{
+            return Database.get(GetIndex(ID)).get(equippedItemIndex);
+        }
+
     }
     //Sets Equipped Item
-    public void SetEquippedItem(int ID, String item) {
+    public void SetEquippedItem(String ID, String item, String EquipItem) {
+        int equippedItemIndex = -1;
+        // Used for QoL, Allows you to enter the location you want changed rather than a number
+        switch(item){
+            case "hand":
+                equippedItemIndex = 2;
+                break;
+            case "armour":
+                equippedItemIndex = 3;
+                break;
+            case "trinket":
+                equippedItemIndex = 4;
+                break;
+        }
+
         for (ArrayList<String> Record : Database) {
             if (Record.get(0).equalsIgnoreCase(valueOf(ID))) {
                 for (String invItem : Record) {
-                    if (invItem.equalsIgnoreCase(item)) {
-                        Record.set(5, item);
+                    if (invItem.equalsIgnoreCase(EquipItem)) {
+                        Record.set(equippedItemIndex, EquipItem);
+
                     }
                 }
             }
@@ -140,10 +188,10 @@ public class Database {
     }
 
     //Used to get the Index of the User's Record
-    static int GetIndex(int ID) {
+    public int GetIndex(String ID) {
         int index = 0;
         for (ArrayList<String> Record : Database){
-            if (Record.get(0).equalsIgnoreCase(valueOf(ID))){
+            if (Record.get(0).equalsIgnoreCase(ID)){
                 return index;
             }
             index++;
@@ -151,3 +199,50 @@ public class Database {
         return -1;
     }
 }
+
+/*
+  //Testing Database
+
+        //Adds Database for each class
+        File path = getApplicationContext().getFilesDir();
+        Database warrior;
+        Database mage;
+        Database cleric;
+        Database ranger;
+
+        try {
+            warrior = new Database("warrior.txt", path);
+            mage = new Database("mage.txt", path);
+            cleric = new Database("cleric.txt", path);
+            ranger = new Database("ranger.txt", path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        //Testing Database
+        try {
+            String USER = "TEST";
+            String USERPASS = "PASS";
+
+            //Creation of a new user, must run all 4, one per class.
+
+            warrior.NewRecord(USER, "PASS");
+            mage.NewRecord(USER, USERPASS);
+            cleric.NewRecord(USER, USERPASS);
+            ranger.NewRecord(USER, USERPASS);
+
+            //Variable(warrior) is for which database/class to pull from
+            //'USER' is which user you want to pull from. *ALL* method parameters start with 'USER'
+            warrior.InventoryAddItem(USER, "sword", 5);
+            warrior.SetEquippedItem(USER, "hand", "sword");
+            String testing = warrior.GetEquippedItem(USER, "hand");
+            String USERSPASSWORD = warrior.Query(USER, 1);
+            String testing2 = "Random extra line for debug screen to stop on";
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        //END TEST
+ */
